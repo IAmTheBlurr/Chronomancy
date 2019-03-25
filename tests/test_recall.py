259488b88e6454020609dcc9d304bd632a23d4b9
Jonathan Craig <blurr@iamtheblurr.com>
2019-03-25 12:01:38 -0700

    Unit Tests -> Arcane Recall -> Adding a new test to ensure that the new target_argument_pos optional parameter functions as expected.

--- tests/test_recall.py
+++ tests/test_recall.py
@@ -26,7 +26,7 @@ class TestRecall(TestCase):
     def setUp(self):
         self.test = Test()
 
-    def becomes_expected(self, current, expected):
+    def becomes_expected(self, current, expected, target_arg_pos=0):
         calling_frame = inspect.stack()[1]
         sleep_time = 0.25
         timeout = 0
@@ -36,7 +36,7 @@ class TestRecall(TestCase):
         while timeout < self.test.max_timeout:
             if not current == expected:
                 sleep(sleep_time)
-                current = recall.arcane_recall(calling_frame)
+                current = recall.arcane_recall(calling_frame, target_argument_pos=target_arg_pos)
                 print('Expected: {0}, Found: {1}'.format(expected, current))
                 timeout += sleep_time
             else:
@@ -50,3 +50,6 @@ class TestRecall(TestCase):
 
     def test_total_recall_works_with_class_methods(self):
         assert self.becomes_expected(self.test.rand_method(), 10)
+
+    def test_total_recall_works_defined_argument_position(self):
+        assert self.becomes_expected(10, self.test.rand_method(), 1)
