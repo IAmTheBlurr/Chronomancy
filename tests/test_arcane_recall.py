18ea517defe4f4f325cb613ff7c5cf9ce9bebc75
Blurr <blurr@iamtheblurr.com>
2025-03-05 14:35:02 -0700

    arcane_recall.py -> adds @deprecated and replaces impl with new class use
    tests included

--- tests/test_arcane_recall.py
+++ tests/test_arcane_recall.py
@@ -1,46 +1,19 @@
+""" Test the arcane_recall function. """
 import inspect
-import random
 
 from time import sleep
 from unittest import TestCase
 
-import chronomancy
+from chronomancy.arcane_recall import arcane_recall
 
-
-class Test(object):
-    """ A simple class to test the arcane_recall function. """
-    def __init__(self):
-        self.number = 10
-        self.max_timeout = 100
-
-    @property
-    def rand_property(self) -> int:
-        """
-        Randomly returns a number between 1 and 10.
-
-        Returns:
-            int: A random number between 1 and 10.
-
-        """
-        return random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-
-    @staticmethod
-    def rand_method() -> int:
-        """
-        Randomly returns a number between 1 and 10.
-
-        Returns:
-            int: A random number between 1 and 10.
-
-        """
-        return random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+from helpers import RandoHelper
 
 
 class TestRecall(TestCase):
 
     def setUp(self):
         """ Setup the test class instance. """
-        self.test = Test()
+        self.rando = RandoHelper()
 
     def becomes_expected(self, current_value, expected_value, target_arg_pos: int = 0) -> bool:
         """
@@ -61,10 +34,10 @@ class TestRecall(TestCase):
 
         found_expected = False
 
-        while timeout < self.test.max_timeout:
+        while timeout < self.rando.max_timeout:
             if current_value != expected_value:
                 sleep(sleep_time)
-                current_value = chronomancy.arcane_recall(calling_frame, target_argument_pos=target_arg_pos)
+                current_value = arcane_recall(calling_frame, target_argument_pos=target_arg_pos)
                 print('Expected: {0}, Found: {1}'.format(expected_value, current_value))
                 timeout += sleep_time
             else:
@@ -74,10 +47,10 @@ class TestRecall(TestCase):
         return found_expected
 
     def test_arcane_recall_works_with_class_instance_properties(self):
-        assert self.becomes_expected(self.test.rand_property, 10)
+        assert self.becomes_expected(self.rando.rand_property, 10)
 
     def test_arcane_recall_works_with_class_methods(self):
-        assert self.becomes_expected(self.test.rand_method(), 10)
+        assert self.becomes_expected(self.rando.rand_method(), 10)
 
     def test_arcane_recall_works_defined_argument_position(self):
-        assert self.becomes_expected(10, self.test.rand_method(), 1)
+        assert self.becomes_expected(10, self.rando.rand_method(), 1)
